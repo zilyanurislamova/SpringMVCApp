e04d263eb776ff91d961bff76897275c9aa40ec2
Zilya <zilya.nurislamova@mail.ru>
2022-08-03 17:59:19 +0300

    create new person and save

--- src/main/java/ru/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/springcourse/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package ru.springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.springcourse.dao.PersonDAO;
+import ru.springcourse.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -28,5 +27,16 @@ public class PeopleController {
     public String getPerson(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDAO.getPerson(id));
         return "people/person";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
--- src/main/java/ru/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/springcourse/dao/PersonDAO.java
@@ -25,4 +25,9 @@ public class PersonDAO {
     public Person getPerson(int id) {
         return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
     }
+
+    public void save(Person person) {
+        person.setId(++PERSON_ID);
+        people.add(person);
+    }
 }
